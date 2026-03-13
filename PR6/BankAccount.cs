0235e71a94c08322ae33e8abfc7a0e85a39a5d8f
ami <aminazemdihanova@mail.ru>
2026-03-13 17:24:10 +0300

    1 тест: снимается ли со счета нужная сумма при допустимом размере кредита

--- PR6/BankAccount.cs
+++ PR6/BankAccount.cs
@@ -70,7 +70,7 @@ namespace BankAccountNS
                 throw new ArgumentOutOfRangeException("amount");
             }
 
-            m_balance += amount;
+            m_balance -= amount;
         }
 
         /// <summary>
