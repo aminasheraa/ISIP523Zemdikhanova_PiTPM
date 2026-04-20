e1748caf8f6ed5388ce7ea9539b0f48862b7ab59
ami <aminazemdihanova@mail.ru>
2026-04-20 15:07:04 +0300

    xml-документирование

--- Letters/Program.cs
+++ Letters/Program.cs
@@ -1,21 +1,36 @@
 ﻿using System;
 
+/// <summary>
+/// Демонстрирует работу с массивами и постепенное формирование строки,
+/// а также отправку сообщений с увеличивающимся счётчиком.
+/// </summary>
 class ArrayExample
 {
+    /// <summary>
+    /// Точка входа в программу.
+    /// Создаёт массив символов, формирует имя и вызывает метод отправки сообщений.
+    /// </summary>
     static void Main()
     {
         char[] letters = { 'f', 'r', 'e', 'd', ' ', 's', 'm', 'i', 't', 'h' };
         string name = "";
         int[] a = new int[10];
+
         for (int i = 0; i < letters.Length; i++)
         {
             name += letters[i];
             a[i] = i + 1;
             SendMessage(name, a[i]);
         }
+
         Console.ReadKey();
     }
 
+    /// <summary>
+    /// Выводит приветственное сообщение с именем и текущим числом.
+    /// </summary>
+    /// <param name="name">Имя, сформированное из массива символов.</param>
+    /// <param name="msg">Число, до которого нужно считать.</param>
     static void SendMessage(string name, int msg)
     {
         Console.WriteLine("Hello, " + name + "! Count to " + msg);
